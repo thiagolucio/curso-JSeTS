2135c8d14e2a30e0473bce92a7c3c191c50cccd7
thiagolucio <THIAGO.SYSMAP@fleetcor.com.br>
2023-08-20 17:33:08 -0300

    atualizando conteudo

--- aula15/index.js
+++ aula15/index.js
@@ -1,8 +1,14 @@
-console.log(!!(100 / 0));
+console.log(!!(100 / 0)); // retorna true porque em JS isso é infinito
 
-// let num2 = Math.floor(num1);
-// let num2 = Math.ceil(num1);
-// let num2 = Math.round(num1);
-// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
-// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
-// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// let num2 = Math.floor(num1); // arredonda pra baixo o numero
+// let num2 = Math.ceil(num1); // arredonda pra cima o numero
+// let num2 = Math.round(num1); // arredonda para o numero mais proximo. Ex: 9.5 = 10 OU 9.4 = 9
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // retorna o maior valor da sequencia
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // retorna o menor valor da sequencia
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // retorna um numero aleatorio com arredondamento
+// console.log(Math.PI); // retorna o valor de PI
+// console.log(Math.pow(2,3)); // retorna o valor de 2 elevado a 3
+// console.log(2 ** 3); // retorna o valor de 2 elevado a 3
+// console.log(Math.sqrt(9)); // retorna o valor de 9 elevado a 1/2
+// console.log(Math.abs(-10)); // retorna o valor absoluto
+// console.log(9 ** 0.5); // retorna o valor da raiz quadrada
--- aula16/index.js
+++ aula16/index.js
@@ -1,27 +1,31 @@
+// Array é um objeto indexado para o javascript
 //               0       1        2          3      4
 let alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo 
-alunos = 123;
+// console.log(typeof alunos);
+// console.log(alunos instanceof Array);
+// alunos = 123;
+alunos.push('Luiza'); // Adiciona no fim
+alunos.push('Eduardo');
 
-console.log(typeof alunos);
-console.log(alunos instanceof Array);
+// alunos.push('Luiza'); // Adiciona no final do array o nome Luiza
+// alunos.push('Eduardo'); // igual acima
+// alunos.unshift('Fábio'); // Adiciona no inicio - primeira posicao
+// alunos.unshift('Luiza');
 
-// alunos.push('Luiza');
-// alunos.push('Eduardo');
-
-// console.log(alunos.slice(0, -2));
+console.log(alunos.slice(0, -3)); // 
 
 // console.log(alunos[50]);
 
 // delete alunos[1];
 // console.log(alunos[1]);
 
-// const removido = alunos.shift();
+// const removido = alunos.shift(); // remove o PRIMEIRO item do array e salva numa const chamda removido
 // console.log(removido);
 // console.log(alunos);
 
-// alunos.unshift('Fábio'); // Adiciona no começo
-// alunos.unshift('Luiza');
-
+// const removido = alunos.pop(); // remove o ULTIMO item do array e salva numa const chamda removido
+// console.log(removido);
+// console.log(alunos);
 
 // alunos.push('Luiza'); // Adiciona no fim
 // alunos.push('Fábio');
@@ -36,3 +40,7 @@ console.log(alunos instanceof Array);
 // console.log(alunos);
 // console.log(alunos[0]);
 // console.log(alunos[2]);
+
+delete alunos[1]; // deleta o indice 1 de alunos e deixa o espaço no array vazio
+console.log(alunos); // [ 'Luiz', <1 empty item>, 'João' ]
+console.log(alunos[1]); // undefined
