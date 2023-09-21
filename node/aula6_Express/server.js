8a789096b100061f926bbbc3b03b2b97624c00d4
thiagolucio <THIAGO.SYSMAP@fleetcor.com.br>
2023-09-21 11:13:12 -0300

    atualizando as aulas - node express e node mooon primeira parte

---
+++ node/aula6_Express/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+
+//         Criar   ler   atualizar apagar
+// CRUD -> CREATE, READ, UPDATE,   DELETE
+//         POST    GET   PUT       DELETE
+
+// http://meusite.com/ <- GET -> Entregue a página "/""
+// http://meusite.com/sobre <- GET -> Entregue a página "/sobre"
+// http://meusite.com/contato <- GET -> Entregue a página "/contato"
+
+app.get('/', (req, res) => {
+  res.send(`
+  <form action="/" method="POST">
+  Nome: <input type="text" name="nome">
+  <button>Enviar</button>
+  </form>
+  `);
+});
+
+
+app.post('/', (req, res) => {
+  res.send('Enviado com sucesso!');
+});
+
+app.get('/contato', (req, res) => {
+  res.send('PAGINA DE CONTATO');
+});
+
+// tratando erro 404
+app.get('*', (req, res) => {
+  res.status(404).send('Esta página não existe!');
+});
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
