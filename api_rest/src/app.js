1e446e2ae1f60a14851b348ba76f2091be00492f
thiagolucio <THIAGO.SYSMAP@fleetcor.com.br>
2023-10-17 16:37:02 -0300

    atualizando segundo projetinho de uma API com Node

--- api_rest/src/app.js
+++ api_rest/src/app.js
@@ -19,13 +19,13 @@ const whiteList = [
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if(whiteList.indexOf(origin) !== -1 || !origin) {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
 };
 
 class App {
